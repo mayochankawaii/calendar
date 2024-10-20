88df0259630ff63a288bcf5e66e783d59c1b8aa2
mayochankawaii <171436876+mayochankawaii@users.noreply.github.com>
2024-10-20 14:28:36 +0000

    カレンダー機能2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "45c0cfc5189c45eeb2c61984634ff0d0.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
