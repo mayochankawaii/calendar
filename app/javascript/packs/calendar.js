d9938f24ede9129af2b1aa301dc07b383338a336
mayochankawaii <171436876+mayochankawaii@users.noreply.github.com>
2024-11-05 02:11:17 +0000

    カレンダー機能7

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -1,14 +1,14 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import listPlugin from "@fullcalendar/list"; //この行を追加
+import listPlugin from "@fullcalendar/list";
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin, listPlugin],　//listPluginを追加
+    plugins: [dayGridPlugin, listPlugin],
     initialView: 'dayGridMonth',
+    locale: "jp", //この行を追加
     events: '/events',
-    //レスポンシブ処理を追加
     windowResize: function () {
         if (window.innerWidth < 991.98) {
             calendar.changeView('listMonth');
